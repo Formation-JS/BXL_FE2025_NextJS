a0df9c2292df58216dcf751eb5ffd8752e563fc1
Gérard Mentor <aurelien.strimelle@bstorm.be>
2025-09-10 10:43:45 +0200

    Demo - ProductService implementation

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -21,6 +21,51 @@ const productService = {
         const product : Product = await response.json();
 
         return product;
+    },
+
+    getMostInStock : async() : Promise<ProductList> => {
+
+        const response : Response =await fetch('http://localhost:4224/products?_sort=stock&_order=desc') ;
+
+        const data : Product[] = await response.json();
+
+        const maxStock = data[0].stock;
+        const datas = data.filter(e => e.stock === maxStock);
+
+        return datas;
+    },
+
+    getCountProduct : async (isFood: boolean) : Promise<number> => {
+
+        const response : Response =await fetch(`http://localhost:4224/products?isFood=${isFood}`);
+        const data : Product[] = await response.json();
+
+        return data.length;
+    },
+
+    getOutOfStock: async () : Promise<ProductList> => {
+        const response : Response =await fetch('http://localhost:4224/products?stock=0');
+        const data : Product[] = await response.json();
+
+        return data;
+    },
+
+    add: async (product: Product) : Promise<number> => {
+       
+        const response : Response  = await fetch('http://localhost:4224/products',{
+            method : 'POST',
+            body : JSON.stringify(product),
+            headers :{
+                'Content-Type':'application/json'
+            }
+        });
+        if(!response.ok){
+            throw new Error('Not created mohamed')
+        }
+
+        const createdProduct : Product = await response.json();
+
+        return createdProduct.id;
     }
 
 };
